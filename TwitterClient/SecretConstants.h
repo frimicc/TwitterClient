62410ddf1fc3243f4c27df148787a208f329bad3
Michael Friedman <frimicc@gmail.com>
2017-01-30 22:23:17 -0800

    Decided to commit SecretConstants.h so others can implement SecretConstants.m with their own API keys.

---
+++ TwitterClient/SecretConstants.h
@@ -0,0 +1,16 @@
+//
+//  SecretConstants.h
+//  TwitterClient
+//
+//  Created by  Michael Friedman on 1/30/17.
+//  Copyright © 2017  Michael Friedman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SecretConstants : NSObject
+
++ (NSString *) getConsumerKey;
++ (NSString *) getConsumerSecret;
+
+@end
